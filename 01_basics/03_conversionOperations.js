9efc3eea511efc2dadb47636cb49d1b65e9c0662
chetanEy2020 <50137228+chetanEy2020@users.noreply.github.com>
2025-07-13 11:20:15 +0000

    Conversion of DataTypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,16 @@
+let score=null//null,""->0
+//console.log(typeof score)
+//console.log(typeof(score))
+
+let ValueInNumber = Number(score)
+//console.log(typeof ValueInNumber)
+//console.log(ValueInNumber) //NaN
+
+//33=> 33
+//"33abc"=> NaN
+//true=> 1, false=>
+    
+let isLoggedIn= "Chetan"
+let booleanIsLoggedIn= Boolean(isLoggedIn)
+console.log(typeof booleanIsLoggedIn)
+console.log(booleanIsLoggedIn)
